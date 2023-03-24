655561a2c2a4da909582028f883ce06813c1c592
Michael Wagner <125706689+MichaelWagner21@users.noreply.github.com>
2023-03-24 17:24:07 -0600

    Commit

---
+++ ProjectComputeThis/ComputeThis.java
@@ -0,0 +1,9 @@
+public class ComputeThis {
+    public static void main(String[] args){
+        double d1 = 3*Math.PI*Math.sin(Math.toRadians(187))+Math.abs(Math.cos(Math.toRadians(122)));
+        double d2 = Math.pow(14.72,3.801)+Math.log(72);
+
+        System.out.println("d1 = "+d1);
+        System.out.print("d2 = "+ d2);
+    }
+}
