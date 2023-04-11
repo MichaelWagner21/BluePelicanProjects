96b551b99108475938158d8e4d22ed106f87a41d
MichaelWagner21 <michaelwagner021@gmail.com>
2023-04-11 11:09:08 -0600

    commit

--- Project...AddThemUp/AddEmUp.java
+++ Project...AddThemUp/AddEmUp.java
@@ -1,23 +1,16 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class AddEmUp {
     public static void main(String[] args){
-        Scanner kb = new Scanner(System.in); 
- 
-        System.out.print("Enter something like 8 + 33 + 1,345 +137 : "); 
-        String s = kb.nextLine( ); //Best to store in a String and then create a new Scanner 
-        //object; otherwise, it can get stuck waiting for input. 
-        Scanner sc = new Scanner(s); 
-        //Set delimiters to a plus sign surrounded by any amount of white space...or... 
-        // a minus sign surrounded by any amount of white space. 
-        sc.useDelimiter("\\s*\\+\\s*"); 
-        //sc.useDelimiter("\\s*\\-\\s*");
-        
-        int sum = 0; 
-        while(sc.hasNextInt( )) 
-        { 
-        sum = sum + sc.nextInt( ); 
-        } 
-        System.out.println("Sum is: " + sum);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter something like 8 + 33 + 1,345 +137 : "); 
+            String inputString = sc.nextLine( ); //Best to store in a String and then create a new Scanner 
+            char[] digits = {'1','2','3','4','5','6','7',};
+            ArrayList<Integer> numList = new ArrayList<Integer>();
+            for (i = 0; i<inputString.length(); i++){
+                currentNumber = 0;
+            }
+        }
+
     }
 }
