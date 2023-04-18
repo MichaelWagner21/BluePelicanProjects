7a14aa690b14dd5ddc09474fb7528039aab597ba
MichaelWagner21 <michaelwagner021@gmail.com>
2023-04-18 14:10:45 -0600

    commit

---
+++ Project...MatrixMultiplication/Tester.java
@@ -0,0 +1,12 @@
+public class Tester {
+    public static void main(String[] args){
+        int a[][] = {{1,2,-2,0},{-3,4,7,2},{6,0,3,1}};
+        int b[][] = {{-1,3},{0,9},{1,-11},{4,-5}};
+    }
+}
+
+class MatrixMult {
+    public static int[][] mult(int[][] matrix1, int[][]matrix2){
+
+    }
+}
